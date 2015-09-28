d1656171406246e5a2d9fe855c6172a6e20e05f4
David Gageot <david@gageot.net>
2015-09-28 09:45:21 +0200

    Extract method

--- mastermind.go
+++ mastermind.go
@@ -1,9 +1,31 @@
 package main
 
+func CountCorrectColorAndPosition(secret [4]string, guess [4]string) int {
+	count := 0
+
+	for i := range secret {
+		if secret[i] == guess[i] {
+			count++
+		}
+	}
+
+	return count
+}
+
 func CountCorrectColorWrongPosition(secret [4]string, guess [4]string) int {
 	count := 0
 
+	colors := allColors(secret, guess)
+	for color := range colors {
+		count += countWrongPosition(color, secret, guess)
+	}
+
+	return count
+}
+
+func allColors(secret [4]string, guess [4]string) map[string]bool {
 	colors := make(map[string]bool)
+
 	for _, color := range secret {
 		colors[color] = true
 	}
@@ -11,11 +33,7 @@ func CountCorrectColorWrongPosition(secret [4]string, guess [4]string) int {
 		colors[color] = true
 	}
 
-	for color := range colors {
-		count += countWrongPosition(color, secret, guess)
-	}
-
-	return count
+	return colors
 }
 
 func countWrongPosition(color string, secret [4]string, guess [4]string) int {
@@ -36,16 +54,4 @@ func countWrongPosition(color string, secret [4]string, guess [4]string) int {
 		return inSecret
 	}
 	return inGuess
-}
-
-func CountCorrectColorAndPosition(secret [4]string, guess [4]string) int {
-	count := 0
-
-	for i := range secret {
-		if secret[i] == guess[i] {
-			count++
-		}
-	}
-
-	return count
 }
